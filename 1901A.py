8884c4605b1bc7e495a57e63d3f7adbb686d16fc
shaekpi <144996414+shaekpi@users.noreply.github.com>
2024-01-21 19:31:38 +0800

    Add files via upload

---
+++ 1901A.py
@@ -0,0 +1,9 @@
+t = int(input())
+
+for _ in range(t):
+    dis = 0
+    n, x = [int(i) for i in input().split(" ")]
+    a = [int(i) for i in input().split(" ")]
+    a.append(x)
+    for pos in range(a) + 1:
+        dis = a[]
